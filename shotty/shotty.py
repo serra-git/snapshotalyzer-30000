78abf5b4f71742288812bd66661b550a1ea07a15
serra-git <serraka@gmail.com>
2019-05-24 00:47:06 +0200

    Filter instances by project. Also stop and start instances

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,11 +4,7 @@ import click
 session = boto3.Session (profile_name = 'shotty')
 ec2 = session.resource('ec2')
 
-@click.command()  # para leer script como comando con argumentos
-@click.option('--project', default=None,
-    help="Only instances for project (tag Project:<name>)")
-def list_instances(project):
-    "List EC2 instances" # se añade al --help
+def filter_instances(project):
     instances = []
 
     if project:
@@ -17,6 +13,21 @@ def list_instances(project):
     else:
         instances = ec2.instances.all()
 
+    return instances
+
+@click.group()
+def instances():
+    """Commands for instances"""
+
+#primer comando list, con su filtro por proyecto. En lugar de @click.command ahora es @instances.command para tener más de un posible comando que pertenece al grupo instances
+
+@instances.command('list')  # para leer script como comando con argumentos
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def list_instances(project):
+    "List EC2 instances" # se añade al --help
+    
+    instances = filter_instances(project)
       
     for i in instances:
         tags ={ t['Key']: t['Value'] for t in i.tags or []}  # dictionary comprehension de: 
@@ -32,5 +43,37 @@ def list_instances(project):
 
     return
 
+#segundo comando stop con su filtro por proyecto
+@instances.command('stop')
+@click.option('--project', default=None,
+    help='Only instances for project')
+
+def stop_instances(project):
+    "Stop EC2 instances"
+
+    instances = filter_instances(project)
+      
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+    
+    return
+
+#tercer comando start con su filtro por proyecto
+@instances.command('start')
+@click.option('--project', default=None,
+    help='Only instances for project')
+
+def stop_instances(project):
+    "Start EC2 instances"
+
+    instances = filter_instances(project)
+      
+    for i in instances:
+        print("Starting {0}...".format(i.id))
+        i.start()
+    
+    return
+
 if __name__ == '__main__':
-    list_instances()
+    instances()
