98c90e7b12b33ff87ee8212b9ac744cc108314ab
serra-git <serraka@gmail.com>
2019-05-25 23:23:10 +0200

    List volumes and snapshots. Naive implementation of snapshot command

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -15,11 +15,82 @@ def filter_instances(project):
 
     return instances
 
-@click.group()
+@click.group() #grupo click principal 'cli' que contiene el resto de grupos 'instances' y 'volumes'
+def cli():
+    """Shotty manages snapshots"""
+
+@cli.group('snapshots')
+def snapshots():
+    """Commands for snapshots"""
+
+@snapshots.command('list')  # para leer script como comando con argumentos
+@click.option('--project', default=None,
+    help="Only snapshots for project (tag Project:<name>)")
+def list_snapshots(project):
+    "List EC2 snapshots" # se añade al --help
+    
+    instances = filter_instances(project)
+
+    for i in instances: 
+        for v in i.volumes.all(): 
+            for s in v.snapshots.all(): 
+                print (",".join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.state,
+                    s.progress,
+                    s.start_time.strftime("%c")
+                )))
+
+    return
+
+@cli.group('volumes')
+def volumes():
+    """Commands for volumes"""
+
+@volumes.command('list')  # para leer script como comando con argumentos
+@click.option('--project', default=None,
+    help="Only volumes for project (tag Project:<name>)")
+def list_volumes(project):
+    "List EC2 volumes" # se añade al --help
+    
+    instances = filter_instances(project)
+
+    for i in instances: 
+        for v in i.volumes.all(): 
+            print(",".join(( 
+                 v.id, 
+                 i.id, 
+                 v.state, 
+                 str(v.size) + "GiB", 
+                 v.encrypted and "Encrypted" or "Not Encrypted" 
+            )))
+
+    return 
+
+@cli.group('instances')
 def instances():
     """Commands for instances"""
 
 #primer comando list, con su filtro por proyecto. En lugar de @click.command ahora es @instances.command para tener más de un posible comando que pertenece al grupo instances
+
+@instances.command('snapshot')
+@click.option('--project', default=None, 
+    help="Only instances for project (tag Project:<name>)")
+def create_snapshot(project):
+    """Create snapshot for EC2 instances"""
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        i.stop()
+        for v in i.volumes.all():
+            print("Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created by SnapshotAlyzer 30000")
+                
+
+    return
 
 @instances.command('list')  # para leer script como comando con argumentos
 @click.option('--project', default=None,
@@ -76,4 +147,4 @@ def stop_instances(project):
     return
 
 if __name__ == '__main__':
-    instances()
+    cli()
